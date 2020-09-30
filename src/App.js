2dc5af905a3ae5fde14a1cf85c6dc125b1127306
FONNN <robertofierrourrutia@gmail.com>
2020-09-30 14:08:32 -0300

    consumir api

--- src/App.js
+++ src/App.js
@@ -1,18 +1,47 @@
-import React from 'react';
+import React, { Component } from 'react';
 
 //1er elemento importado from Buscador.js
+//en el componente buscador utilizare el prop (pasar elemento de padre a hijo)
+//state es un objeto que tiene una llave y un valor
 import Buscador from './componentes/Buscador';
 
-function App() {
-  return (
-    <div className="app container">
-      <div className="jumbotron">
-        <p className="lead text-center">Buscador</p>
+class App extends Component {
 
-        <Buscador />
+  state = {
+    termino: ''
+  }
+
+  consultarApi = () => {
+    const termino = this.state.termino;
+    const url = `https://simple.ripley.cl/api/v2/products?format=json&partNumbers=2000379450763${termino}`;
+
+    fetch(url)
+      .then(respuesta => respuesta.json())
+      .then(resultado => console.log(resultado))
+
+  }
+
+  //nuevo metodo al componente principal para enviar lo del buscador a la consola
+  datosBusqueda = (termino) => {
+    this.setState({
+      termino
+    })
+  }
+
+  render() {
+    return (
+      <div className="app container">
+        <div className="jumbotron">
+          <p className="lead text-center">Buscador</p>
+      
+          <Buscador 
+            datosBusqueda={this.datosBusqueda}
+          />
+        </div>
+        {this.state.termino}
       </div>
-    </div>
-  );
+    );
+  }
 }
 
 export default App;
--- src/componentes/Buscador.js
+++ src/componentes/Buscador.js
@@ -1,17 +1,33 @@
 import React, { Component } from 'react';
 
-//designo clase buscador
-//render indica que imprimo en el componente
+//1designo clase buscador
+//2render indica que imprimo en el componente
+//creo ref antes de render (utilizarlo en el placeholder del buscador) 
 class Buscador extends Component {
+
+    busquedaRef = React.createRef();
+
+    //en form agrego el evento onSubmit para la f(x) obtenerDatos
+    obtenerDatos = (e) => {
+        e.preventDefault();
+
+        //tomo el valor del input
+        const termino = this.busquedaRef.current.value;
+
+        //los envío al componente principal (app.js)
+        this.props.datosBusqueda(termino);
+    }
+
+    //para imprimir el prop de App.js/buscador lo agrego bajo el row
     render() { 
         return ( 
-            <form>
+            <form onSubmit={this.obtenerDatos}>
                 <div className="row">
                     <div className="form-group col-md-8">
-                        <input type="text" className="form-control form-control-md" placeholder="Buscar. Ejemplo: Notebook" />
+                        <input ref={this.busquedaRef} type="text" className="form-control form-control-md" placeholder="Buscar. Ejemplo: Notebook" />
                     </div>
                     <div className="form-group col-md-4">
-                        <input type="submit" className="btn btn-md btn-danger btn-block" value="Buscar" />
+                        <input type="submit" className="btn btn-md btn-danger btn-block" value="Buscar..." />
                     </div>
                 </div>
             </form>
